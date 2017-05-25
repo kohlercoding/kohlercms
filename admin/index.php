38981a424fcef0422a2bcd032b6826563201226c
KohlerCoding <mikkel@kohlercoding.com>
2017-05-25 22:29:46 +0200

    edited template and changed linking system

--- admin/index.php
+++ admin/index.php
@@ -14,6 +14,17 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap-theme-cyborg.min.css">
 	<link rel="stylesheet" type="text/css" href="assets/css/dashboard.css">
+	<script src="https://use.fontawesome.com/7392fcfc82.js"></script>
+	<script src="https://code.jquery.com/jquery-3.1.1.min.js""></script>
+	<script>
+		$.ajax({
+			url: 'https://randomuser.me/api/?results=1&inc=picture&nat=DK&gender=male',
+			dataType: 'json',
+			success: function(data) {
+				$("#profileImg").attr("src", data.results[0].picture.medium);
+			}
+		})
+	</script>
 	<style>
 		/*body {
 			padding-top: 3.4rem;
@@ -22,6 +33,9 @@
 		.container {
 			background: #fff;
 		}*/
+		#profileImg {
+			border-radius: 50%;
+		}
 	</style>
 
 	<!-- Page Title -->
@@ -32,7 +46,7 @@
 		<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
-		<a class="navbar-brand" href="#">Navbar</a>
+		<a class="navbar-brand" href="#">Housekeeping</a>
 
 		<div class="collapse navbar-collapse" id="navbarColor01">
 			<ul class="navbar-nav mr-auto">
@@ -40,19 +54,30 @@
 					<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link" href="#">Features</a>
+					<a class="nav-link" href="#"><i class="fa fa-users"></i> Users</a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link" href="#">Pricing</a>
+					<a class="nav-link" href="#"><i class="fa fa-puzzle-piece"></i> Plugins (WIP)</a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link" href="#">About</a>
+					<a class="nav-link" href="#"><i class="fa fa-cogs"></i> Site settings</a>
 				</li>
 			</ul>
-			<form class="form-inline">
-				<input class="form-control mr-sm-2" type="text" placeholder="Search">
-				<button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-			</form>
+			<ul class="navbar-nav navbar-right">
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						<img id="profileImg" style="margin-top: -0.5em;margin-bottom: -0.5em;" src="" height="30" width="30" class="d-inline-block align-middle">&nbsp;
+						Your Profile
+					</a>
+					<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+						<a href="#" class="dropdown-item">Update information</a>
+						<a href="#" class="dropdown-item">Log out</a>
+					</div>
+				</li>
+				<li class="nav-item">
+					<a href="#" class="nav-link">Back to site</a>
+				</li>
+			</ul>
 		</div>
 	</nav>
 
@@ -61,200 +86,60 @@
 			<nav class="col-sm-3 col-md-2 hidden-xs-down bg-inverse sidebar">
 				<ul class="nav nav-pills flex-column">
 					<li class="nav-item">
-						<a class="nav-link active" href="#">Overview <span class="sr-only">(current)</span></a>
+						<i class="nav-link fa fa-info-circle fa-2x"></i>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link" href="#">Reports</a>
+						<a class="nav-link" href="?p=home">Overview</a>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link" href="#">Analytics</a>
+						<a class="nav-link" href="?p=adm_navbar">Nav items</a>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link" href="#">Export</a>
-					</li>
-				</ul>
-
-				<ul class="nav nav-pills flex-column">
-					<li class="nav-item">
-						<a class="nav-link" href="#">Nav item</a>
+						<a class="nav-link" href="#">Pages</a>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link" href="#">Nav item again</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#">One more nav</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#">Another nav item</a>
+						<a class="nav-link" href="#">Content</a>
 					</li>
 				</ul>
 
 				<ul class="nav nav-pills flex-column">
 					<li class="nav-item">
-						<a class="nav-link" href="#">Nav item again</a>
+						<i class="nav-link fa fa-users fa-2x"></i>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link" href="#">One more nav</a>
+						<a class="nav-link" href="#">Edit users</a>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link" href="#">Another nav item</a>
+						<a class="nav-link" href="#">Global user settings</a>
+					</li>
+				</ul>
+
+				<ul class="nav nav-pills flex-column">
+					<li class="nav-item">
+						<i class="nav-link fa fa-wrench fa-2x"></i>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Site settings</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Permalinks</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">And more...</a>
 					</li>
 				</ul>
 			</nav>
 			<main class="col-sm-3 offset-sm-3 col-md-10 offset-md-2 pt-3">
-				<h1>Dashboard</h1>
-
-				<section class="row text-center placeholders">
-					<div class="col-6 col-sm-3 placeholder">
-						<img src="data:image/gif;base64,R0lGODlhAQABAIABAAJ12AAAACwAAAAAAQABAAACAkQBADs=" width="200" height="200" class="img-fluid rounded-circle" alt="Generic placeholder thumbnail">
-						<h4>64%</h4>
-						<div class="text-muted">Users</div>
-					</div>
-					<div class="col-6 col-sm-3 placeholder">
-						<img src="data:image/gif;base64,R0lGODlhAQABAIABAADcgwAAACwAAAAAAQABAAACAkQBADs=" width="200" height="200" class="img-fluid rounded-circle" alt="Generic placeholder thumbnail">
-						<h4>32%</h4>
-						<span class="text-muted">Orders</span>
-					</div>
-					<div class="col-6 col-sm-3 placeholder">
-						<img src="data:image/gif;base64,R0lGODlhAQABAIABAAJ12AAAACwAAAAAAQABAAACAkQBADs=" width="200" height="200" class="img-fluid rounded-circle" alt="Generic placeholder thumbnail">
-						<h4>16%</h4>
-						<span class="text-muted">RMAs</span>
-					</div>
-					<div class="col-6 col-sm-3 placeholder">
-						<img src="data:image/gif;base64,R0lGODlhAQABAIABAADcgwAAACwAAAAAAQABAAACAkQBADs=" width="200" height="200" class="img-fluid rounded-circle" alt="Generic placeholder thumbnail">
-						<h4>8%</h4>
-						<span class="text-muted">Income</span>
-					</div>
-				</section>
-
-				<h2>Customers</h2>
-				<div class="table-responsive">
-					<table class="table table-striped">
-						<thead>
-							<tr>
-								<th>#</th>
-								<th>Header</th>
-								<th>Header</th>
-								<th>Header</th>
-								<th>Header</th>
-							</tr>
-						</thead>
-						<tbody>
-							<tr>
-								<td>1,001</td>
-								<td>Lorem</td>
-								<td>ipsum</td>
-								<td>dolor</td>
-								<td>sit</td>
-							</tr>
-							<tr>
-								<td>1,002</td>
-								<td>amet</td>
-								<td>consectetur</td>
-								<td>adipiscing</td>
-								<td>elit</td>
-							</tr>
-							<tr>
-								<td>1,003</td>
-								<td>Integer</td>
-								<td>nec</td>
-								<td>odio</td>
-								<td>Praesent</td>
-							</tr>
-							<tr>
-								<td>1,003</td>
-								<td>libero</td>
-								<td>Sed</td>
-								<td>cursus</td>
-								<td>ante</td>
-							</tr>
-							<tr>
-								<td>1,004</td>
-								<td>dapibus</td>
-								<td>diam</td>
-								<td>Sed</td>
-								<td>nisi</td>
-							</tr>
-							<tr>
-								<td>1,005</td>
-								<td>Nulla</td>
-								<td>quis</td>
-								<td>sem</td>
-								<td>at</td>
-							</tr>
-							<tr>
-								<td>1,006</td>
-								<td>nibh</td>
-								<td>elementum</td>
-								<td>imperdiet</td>
-								<td>Duis</td>
-							</tr>
-							<tr>
-								<td>1,007</td>
-								<td>sagittis</td>
-								<td>ipsum</td>
-								<td>Praesent</td>
-								<td>mauris</td>
-							</tr>
-							<tr>
-								<td>1,008</td>
-								<td>Fusce</td>
-								<td>nec</td>
-								<td>tellus</td>
-								<td>sed</td>
-							</tr>
-							<tr>
-								<td>1,009</td>
-								<td>augue</td>
-								<td>semper</td>
-								<td>porta</td>
-								<td>Mauris</td>
-							</tr>
-							<tr>
-								<td>1,010</td>
-								<td>massa</td>
-								<td>Vestibulum</td>
-								<td>lacinia</td>
-								<td>arcu</td>
-							</tr>
-							<tr>
-								<td>1,011</td>
-								<td>eget</td>
-								<td>nulla</td>
-								<td>Class</td>
-								<td>aptent</td>
-							</tr>
-							<tr>
-								<td>1,012</td>
-								<td>taciti</td>
-								<td>sociosqu</td>
-								<td>ad</td>
-								<td>litora</td>
-							</tr>
-							<tr>
-								<td>1,013</td>
-								<td>torquent</td>
-								<td>per</td>
-								<td>conubia</td>
-								<td>nostra</td>
-							</tr>
-							<tr>
-								<td>1,014</td>
-								<td>per</td>
-								<td>inceptos</td>
-								<td>himenaeos</td>
-								<td>Curabitur</td>
-							</tr>
-							<tr>
-								<td>1,015</td>
-								<td>sodales</td>
-								<td>ligula</td>
-								<td>in</td>
-								<td>libero</td>
-							</tr>
-						</tbody>
-					</table>
-				</div>
+				<?php 
+					if(!isset($_GET['p'])) {
+						header("location: index.php?p=home");
+						$file = $_GET['p'].".php";
+						include $file;
+					} else {
+						$file = $_GET['p'].".php";
+						include $file;
+					}
+				?>
 			</main>
 		</div>
 	</div><!-- /.container-fluid -->
